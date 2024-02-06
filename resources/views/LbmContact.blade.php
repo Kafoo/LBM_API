7c4b74181d9f5f77ac3f2bac84c78c45bb50acda
Antoine <ant.guillard@gmail.com>
2024-02-06 10:30:08 +0100

    Mail view update

--- resources/views/LbmContact.blade.php
+++ resources/views/LbmContact.blade.php
@@ -7,8 +7,8 @@
   margin-bottom:35px;
   ">
     <h2>Bonjour Nadia !</h2>
-    <h2>Tu as une nouvelle demande de contact sur LesBonnesManières.paris</h2>
-    <h3><i>Le {!! $infos['date'] !!} à {!! $infos['time'] !!}</i></h3>
+    <h2 style="margin-bottom:0;">Tu as une nouvelle demande de contact sur LesBonnesManières.paris</h2>
+    <h3 style="margin-top:5px;"><i>Le {!! $infos['date'] !!} à {!! $infos['time'] !!}</i></h3>
     <p><b>Nom - Prénom : </b>{!! $infos['name'] !!}</p>
     <p><b>Email : </b>{!! $infos['email'] !!}</p>
     <p><b>Téléphone : </b>{!! $infos['phone'] !!}</p>
@@ -25,7 +25,7 @@
   letter-spacing: 0.1em;
   font-style: italic;
   ">
-    <p>kafoo.dev - Antoine Guillard</p>
+    <p>kafoo . dev - Antoine Guillard</p>
     <p>ant.guillard@gmail.com</p>
     <p>+33 6 42 40 29 16</p>
   </div>
