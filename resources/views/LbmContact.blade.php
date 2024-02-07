ba4a06b26f56b5df6fdf70761fe89930d7b8e3ad
Antoine <ant.guillard@gmail.com>
2024-02-07 10:43:42 +0100

    Mail view update

--- resources/views/LbmContact.blade.php
+++ resources/views/LbmContact.blade.php
@@ -6,9 +6,15 @@
   color: #3A3D51;
   margin-bottom:35px;
   ">
+
     <h2>Bonjour Nadia !</h2>
-    <h2 style="margin-bottom:0;">Tu as une nouvelle demande de contact sur LesBonnesManières.paris</h2>
-    <h3 style="margin-top:5px;"><i>Le {!! $infos['date'] !!} à {!! $infos['time'] !!}</i></h3>
+    <h2 style="margin-bottom:0;">
+      Tu as une nouvelle demande de contact sur LesBonnesManières.paris
+    </h2>
+    <h3 style="margin-top:5px; margin-bottom:30px;"
+      ><i>Le {!! $infos['date'] !!} à {!! $infos['time'] !!}</i>
+    </h3>
+
     <p><b>Nom - Prénom : </b>{!! $infos['name'] !!}</p>
     <p><b>Email : </b>{!! $infos['email'] !!}</p>
     <p><b>Téléphone : </b>{!! $infos['phone'] !!}</p>
@@ -16,7 +22,9 @@
     <p><b>Nombre d'invités : </b>{!! $infos['guest'] !!}</p>
     <p><b>Date de l'événement : </b>{!! $infos['eventDate'] !!}</p>
     <p><b>Message : </b>{!! $infos['message'] !!}</p>
+
   </div>
+
   <div style="
   line-height:0.5em;
   font-size:0.7em;
@@ -29,5 +37,6 @@
     <p>ant.guillard@gmail.com</p>
     <p>+33 6 42 40 29 16</p>
   </div>
+  
 </body>
 </html>
